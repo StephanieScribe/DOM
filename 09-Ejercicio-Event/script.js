64bad10050aaff6a76b3c9bdb9e1ba44480d38d2
Your Name <you@example.com>
2025-01-20 17:11:50 -0600

    Ejercicio Event

---
+++ 09-Ejercicio-Event/script.js
@@ -0,0 +1,48 @@
+/* Referencia */
+
+const emojiCursor = document.querySelector(".emoji-cursor");
+
+/* Detectamos el evento movimiento del mouse */
+
+document.addEventListener("mousemove" , (e) => {
+/*     console.log(e.clientX);
+    console.log(e.clientY); */
+
+    
+    /* Guardamos las coordenadas en variables */
+    let mouseX = e.clientX;
+    let mouseY = e.clientY;
+
+    /* Aplicar las coordenadas al elemento emojiCursor */
+
+    emojiCursor.style.left = `${mouseX}px`;
+    emojiCursor.style.top = `${mouseY}px`;
+    
+
+} );
+
+console.log(emojiCursor.style);
+
+/* Cmabiar el emoji al presionar una tecla */
+
+document.addEventListener("keydown", (e) => {
+    /* Utilizamos Switch para asignar un nuevo emoji */
+
+    switch (e.key) {
+        case "1":
+            emojiCursor.textContent = "🌺";
+            break;
+        case "2":
+            emojiCursor.textContent = "👻";
+            break;
+        case "3":
+            emojiCursor.textContent = "🐷";
+            break;
+        case "4":
+            emojiCursor.textContent = "🍥";
+            break;
+        case "5":
+            emojiCursor.textContent = "🌸";
+            break;
+    }
+});
